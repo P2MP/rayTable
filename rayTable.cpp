65d7a9d49044980bf4b8bca2a1adf78b36d83c18
OmsiQ <fgfmnycz@gmail.com>
2015-11-23 22:11:11 +0100

    Added marking chosen index with "*". Changed function gotoxy(); now requires variable COORD instead of int. Added function getxy().

--- rayTable.cpp
+++ rayTable.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-const short tsize_x = 20;
-const short tsize_y = 10;
+const short tsize_x = 30;
+const short tsize_y = 15;
 string rayTable[tsize_y][tsize_x];
 
 int getx()
@@ -24,27 +24,66 @@ int gety()
     GetConsoleScreenBufferInfo(h,&csbi);
     return csbi.dwCursorPosition.Y;
 }
-void gotoxy(int x, int y)
+COORD getxy()
 {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(h,&csbi);
+    return csbi.dwCursorPosition;
+}
+void gotoxy(COORD coord)
+{
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
-int validation(string convert)
+int strToCoordX(string str)
 {
-    int i;
-    stringstream ss(convert);
-    ss >> i;
-    if (ss.fail())
+    char p0[100];
+    int coord = -1;
+    for (int i = 0; i < tsize_x; i++)
     {
-        return 0;
+        if (i < 10)
+        {
+            p0[i] = i + 48;
+        }
+        if (i >= 10 && i < 36)
+        {
+            p0[i] = i + 87;
+        }
+        if (i >= 36)
+        {
+            p0[i] = i + 29;
+        }
+        if (str[0] == p0[i])
+        {
+            coord = i;
+        }
     }
-    else
+    return coord;
+}
+int strToCoordY(string str)
+{
+    char p0[100];
+    int coord = -1;
+    for (int i = 0; i < tsize_y; i++)
     {
-        return i;
+        if (i < 10)
+        {
+            p0[i] = i + 48;
+        }
+        if (i >= 10 && i < 36)
+        {
+            p0[i] = i + 87;
+        }
+        if (i >= 36)
+        {
+            p0[i] = i + 29;
+        }
+        if (str[0] == p0[i])
+        {
+            coord = i;
+        }
     }
-
+    return coord;
 }
 void rayTablePrint()
 {
@@ -97,11 +136,52 @@ void rayTableClear()
             rayTable[i][j] = '.';
         }
     }
-
 }
 int main()
 {
+    string xIn,yIn;
+    int xCoord,yCoord;
+    COORD cursorPos;
     rayTableClear();
     rayTablePrint();
+    cursorPos = getxy();
+    for(int j = 0; j == 0;)
+    {
+        do
+        {
+            cout << endl << "Podaj wspolrzedna x: ";
+            xIn = getch();
+            cout << xIn << endl;
+            cout << "Podaj wspolrzedna y: ";
+            yIn = getch();
+            cout << yIn << endl;
+            xCoord = strToCoordX(xIn);
+            yCoord = strToCoordY(yIn);
+            if(xCoord == -1 || yCoord == -1)
+            {
+                gotoxy(cursorPos);
+                for(int i = 0; i < 400; i++)
+                {
+                    cout << " ";
+                }
+                gotoxy(cursorPos);
+                cout << endl << "Blad";
+            }else
+            {
+                gotoxy(cursorPos);
+                for(int i = 0; i < 400; i++)
+                {
+                    cout << " ";
+                }
+                rayTable[yCoord][xCoord] = '*';
+            }
+        }
+        while(xCoord == -1 || yCoord == -1);
+        COORD coord00;
+        coord00.X = 0;
+        coord00.Y = 0;
+        gotoxy(coord00);
+        rayTablePrint();
+    }
     return 0;
 }
